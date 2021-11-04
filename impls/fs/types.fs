d835a997e0add9c8cc3d87518714be6b42201082
fluxehub <53355311+fluxehub@users.noreply.github.com>
2021-11-04 20:39:13 +0000

    Don't override arithmetic operators

--- impls/fs/types.fs
+++ impls/fs/types.fs
@@ -5,17 +5,22 @@ type MalType =
     | MalNumber of int
     | MalNil
     | MalBool of bool
+    | MalString of string
     | MalSymbol of string
     | MalFn of (MalType list -> MalType)
-
-let unwrapNumber = function
-    | MalNumber n -> n
-    | s -> failwith $"Error: {s} is not a number"
-
-let (+) a b = MalNumber <| unwrapNumber a + unwrapNumber b
-
-let (-) a b = MalNumber <| unwrapNumber a - unwrapNumber b
-
-let (*) a b = MalNumber <| unwrapNumber a * unwrapNumber b
+with
+    static member unwrapNumber = function
+        | MalNumber n -> n
+        | s -> failwith $"Error: {s} is not a number"
     
-let (/) a b = MalNumber <| unwrapNumber a / unwrapNumber b
+    static member (+) (a, b) =
+        MalNumber <| MalType.unwrapNumber a + MalType.unwrapNumber b
+        
+    static member (-) (a, b) =
+        MalNumber <| MalType.unwrapNumber a + MalType.unwrapNumber b
+        
+    static member (*) (a, b) =
+        MalNumber <| MalType.unwrapNumber a + MalType.unwrapNumber b
+
+    static member (/) (a, b) =
+        MalNumber <| MalType.unwrapNumber a + MalType.unwrapNumber b
