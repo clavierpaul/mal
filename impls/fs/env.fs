47b10328c5eee0ec401a5f1a80387d2611231c28
fluxehub <53355311+fluxehub@users.noreply.github.com>
2021-11-13 23:56:47 +0000

    Removed state monad in favor of simple mutability

--- impls/fs/env.fs
+++ impls/fs/env.fs
@@ -6,51 +6,40 @@ open State
 type ReplEnv = Map<string, MalType>
 
 type Env =
-    { outer: Env option
-      data: ReplEnv }
+    { Outer: Env option
+      mutable Data: ReplEnv }
     
 module Env =
     let ofMap map = 
-        { outer = None
-          data = map }
+        { Outer = None
+          Data = map }
     
-    let ofEnv env = 
-        { outer = Some env
-          data = Map.empty }
+    let fromOuter env = 
+        { Outer = Some env
+          Data = Map.empty }
 
-    let set key value =
-        let set' env =
-            { env with data = Map.add key value env.data }
-        
-        State (fun env -> (), set' env)
-
-    let rec private find' key env =
-        match env.data |> Map.tryFind key with
+    let set key value env =
+        env.Data <- env.Data |> Map.add key value
+    
+    let rec find key env =
+        match env.Data |> Map.tryFind key with
         | Some _ -> (Some env)
         | None ->
-            match env.outer with
-            | Some outer -> find' key outer
+            match env.Outer with
+            | Some outer -> find key outer
             | None -> None
 
-    let find key = State (fun env -> (find' key env), env)
-
-    let get key =
-        let rec get' env =
-            match find' key env with
-            | Some e -> (e.data |> Map.find key), env
-            | None -> failwith $"'{key}' not found"
-        
-        State get'
+    let rec get key env =
+        match find key env with
+        | Some e -> e.Data |> Map.find key
+        | None -> failwith $"'{key}' not found"
     
     let init binds exprs =
-        let paired = List.map2 (fun b e -> (b, e)) binds exprs
-        let setter (k, v) =
-            state {
-                do! set k v
-                return ()
-            }
+        let setter env (k, v) =
+            env |> set k v
         
-        state {
-            let! _ = paired |> State.mapList setter
-            return ()
-        }
+        let paired = List.map2 (fun b e -> (b, e)) binds exprs
+        let env = { Outer = None; Data = Map.empty }
+
+        do List.iter (setter env) paired
+        env
--- impls/fs/step3_env.fs
+++ impls/fs/step3_env.fs
@@ -16,102 +16,74 @@ let repl_env: Env =
 
 let READ = Reader.read_str
 
-let rec EVAL ast: State<Env, MalType> =
-    state {
-        match ast with
-        | MalList    l -> return! eval_list l
-        | MalVector  v ->
-            let! v = State.mapList EVAL v
-            return MalVector v
-        | MalHashmap h ->
-            let mapEval (k, ast) = state {
-                let! v = EVAL ast
-                return (k, v)
-            }
+let rec EVAL env ast: MalType =
+    match ast with
+    | MalList    l -> eval_list env l
+    | MalVector  v -> MalVector  <| List.map (EVAL env) v
+    | MalHashmap h -> MalHashmap <| Map.map (fun _ -> EVAL env) h
+    | ast -> eval_ast env ast 
+
+and addBinding env (key, value) =
+    let key = unwrapSymbol key
+    let evaluated = EVAL env value
+    do env |> Env.set key evaluated
+    evaluated
+
+and eval_list env l =
+    match l with
+    | [] -> MalList []
+    | MalSymbol "def!"::tail ->
+        match tail with
+        | [key; value] -> addBinding env (key, value)
+        | _ -> failwith "def! expected 2 arguments"
+    | MalSymbol "let*"::tail ->
+        match tail with
+        | [bindings; expr]->
+            let bindings = unwrapSeq bindings
+            if List.length bindings % 2 <> 0 then
+                failwith "Binding list contains an odd number of elements"
+            else
+                let subEnv = Env.fromOuter env
+                let bindings = pair bindings
+                do bindings |> List.map (addBinding subEnv) |> ignore
+                EVAL subEnv expr
+        | _ -> failwith "let* expected 2 arguments"
+    | l ->
+        let result = eval_ast env <| MalList l
+        let newList = unwrapList result
+        
+        match List.head newList with
+        | MalFn fn -> fn <| List.tail newList
+        | s        -> failwith $"\"{Printer.pr_str true s}\" is not a function"
             
-            // This code is very efficient!
-            let l = Map.toList h
-            let! nl = State.mapList mapEval l
-            return MalHashmap <| Map.ofList nl
-        | ast -> return! eval_ast ast 
-    }
-
-and addBinding (key, value) =
-    state {
-        let key = unwrapSymbol key
-        let! evaluated = EVAL value
-        do! Env.set key evaluated
-        return evaluated
-    }
-
-and eval_list l =
-    state {
-        match l with
-        | [] -> return MalList []
-        | l  ->
-            match l with
-            | MalSymbol "def!"::tail ->
-                if List.length tail < 2 then
-                    return failwith "def! requires 2 arguments"
-                else
-                    return! addBinding (tail.[0], tail.[1])
-            | MalSymbol "let*"::tail ->
-                if List.length tail < 2 then
-                    return failwith "let* requires 2 arguments"
-                else
-                    let! currentEnv = State.get
-                    let bindings = unwrapSeq tail.[0]
-                    if List.length bindings % 2 <> 0 then
-                        return failwith "Binding list contains an odd number of elements"
-                    else
-                        do! State.put <| Env.ofEnv currentEnv
-                        let bindings = pair bindings
-                        let! _ = bindings |> State.mapList addBinding
-                        let! result = EVAL tail.[1]
-                        do! State.put currentEnv
-                        return result
-            | l ->
-                let! result = eval_ast <| MalList l
-                let newList = unwrapList result
-                
-                match List.head newList with
-                | MalFn fn -> return fn <| List.tail newList
-                | s        -> return failwith $"\"{Printer.pr_str true s}\" is not a function"
-    }
-
-and eval_ast ast =
-    state {
-        match ast with
-        | MalSymbol s -> return! Env.get s
-        | MalList l   -> let! nl = l |> State.mapList EVAL in return MalList nl
-        | ast         -> return ast
-    }
+and eval_ast env ast =
+    match ast with
+    | MalSymbol s -> env |> Env.get s
+    | MalList l   -> let nl = l |> List.map (EVAL env) in MalList nl
+    | ast         -> ast
     
 let PRINT = Printer.pr_str
 
-let rep input env =
+let rep input =
     match input |> READ with
-    | Some program ->
-        let result, newEnv = State.run env (EVAL program)
-        (PRINT true result), newEnv
-    | None -> "", env
+    | Some program -> EVAL repl_env program |> PRINT true
+    | None -> ""
 
-let rec programLoop env =
+let rec programLoop () =
     printf "user> "
     let input = Console.ReadLine ()
     match input with
     | null -> ()
     | s ->
         try
-            let output, newEnv = rep s env
-            printfn $"{output}"
-            programLoop newEnv
+            printfn $"{rep s}"
         with
             | Failure msg ->
                 error $"{msg}"
-                programLoop env
+                
+        programLoop ()
 
 [<EntryPoint>]
 let main _ =
-    programLoop repl_env
+    programLoop ()
     0
